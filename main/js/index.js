b6b76b86fdb346f71197fe1107f0933e02cab98d
sridhant <58651073+sridhant@users.noreply.github.com>
2021-06-07 17:14:18 +0530

    first commit

--- main/js/index.js
+++ main/js/index.js
@@ -4,18 +4,40 @@
  const burgerImg = document.querySelector('#burgerImg')
  let openClose = true;
  const ul = document.querySelector('.navBarLinks') 
+ const audioSrc = document.querySelector('#audioSrc')
 
  burgerDiv.addEventListener('click' , imgChange = (e) => {
      if(openClose == true){
          burgerImg.src = "main/img/open.svg"
+         ul.style.display = "block"
          ul.style.left = "0"
+         ul.setAttribute('class' , 'navBarLinks animate__animated animate__bounceInDown')
          openClose = false;
+         if(audioSrc.paused){
+             audioSrc.play()
+         }
      }
      else if (openClose == false){
         burgerImg.src = "main/img/unOpen.svg"
+        ul.style.display = "none"
         ul.style.left = "100vw"
-        openClose = true;
+        openClose = true;    
+        if(audioSrc.paused){
+            audioSrc.play()
+        }
      }
  })
 
- console.log(burgerImg.src)
+
+ //typing
+ var typed = new Typed('#typed', {
+    strings: [
+        'App', 
+        'Software',
+        'Program',
+    ],
+    smartBackspace: true ,
+    loop : true,
+    typeSpeed: 100,
+    backSpeed: 100,
+  });
